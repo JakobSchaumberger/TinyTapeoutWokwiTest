1d110fb24c3c95d2d56b6ea81acbc4c6ad43d0c2
Jakob Schaumberger <Jakob.Schaumberger@outlook.com>
2025-11-03 09:52:10 +0100

    fixed missing ',' error in tt_um_tinytone.v

--- src/tt_um_tinytone.v
+++ src/tt_um_tinytone.v
@@ -30,7 +30,7 @@ module tt_um_tinytone
 
     // outputs
     output wire [7:0] display_o,        // outputs connect to 7-segment-display
-    output wire [7:0] uo_out            // outputs connected to Pin-Header
+    output wire [7:0] uo_out,           // outputs connected to Pin-Header
     output wire [7:0] uio_out,          // IOs: Output path
 
 );
