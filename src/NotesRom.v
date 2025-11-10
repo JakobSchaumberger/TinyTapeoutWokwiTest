367ca445f9968f8092ba6535a5f9ab6b875bf019
Jakob Schaumberger <Jakob.Schaumberger@outlook.com>
2025-11-10 12:01:23 +0100

    halfed NotesRom to 32 notes

--- src/NotesRom.v
+++ src/NotesRom.v
@@ -14,7 +14,7 @@ module NotesRom
 # (
     parameter BW = 16
 )(
-    input wire [5:0]    note_index_i,       // 64 possible notes
+    input wire [4:0]    note_index_i,       // 32 possible notes
     output reg [BW-1:0] divider_value_o
 );
 
@@ -52,38 +52,38 @@ always @(*) begin
         6'd29: divider_value_o = 24'd8591;   // F5
         6'd30: divider_value_o = 24'd7648;   // G5
         6'd31: divider_value_o = 24'd0;
-        6'd32: divider_value_o = 24'd9097;   // E5
-        6'd33: divider_value_o = 24'd0;
-        6'd34: divider_value_o = 24'd11465;  // C5
-        6'd35: divider_value_o = 24'd10213;  // D5
-        6'd36: divider_value_o = 24'd12151;  // B4
-        6'd37: divider_value_o = 24'd0;
-        6'd38: divider_value_o = 24'd0;
-        6'd39: divider_value_o = 24'd11465;  // C5
-        6'd40: divider_value_o = 24'd15306;  // G4
-        6'd41: divider_value_o = 24'd18196;  // E4
-        6'd42: divider_value_o = 24'd13636;  // A4
-        6'd43: divider_value_o = 24'd0;
-        6'd44: divider_value_o = 24'd13636;  // A4
-        6'd45: divider_value_o = 24'd13636;  // A4
-        6'd46: divider_value_o = 24'd0;
-        6'd47: divider_value_o = 24'd15306;  // G4
-        6'd48: divider_value_o = 24'd0;
-        6'd49: divider_value_o = 24'd0;
-        6'd50: divider_value_o = 24'd9097;   // E5
-        6'd51: divider_value_o = 24'd11465;  // C5
-        6'd52: divider_value_o = 24'd12151;  // B4
-        6'd53: divider_value_o = 24'd13636;  // A4
-        6'd54: divider_value_o = 24'd15306;  // G4
-        6'd55: divider_value_o = 24'd7648;   // G5
-        6'd56: divider_value_o = 24'd9097;   // E5
-        6'd57: divider_value_o = 24'd0;
-        6'd58: divider_value_o = 24'd11465;  // C5
-        6'd59: divider_value_o = 24'd12151;  // B4
-        6'd60: divider_value_o = 24'd13636;  // A4
-        6'd61: divider_value_o = 24'd15306;  // G4
-        6'd62: divider_value_o = 24'd0;
-        6'd63: divider_value_o = 24'd0;
+        // 6'd32: divider_value_o = 24'd9097;   // E5
+        // 6'd33: divider_value_o = 24'd0;
+        // 6'd34: divider_value_o = 24'd11465;  // C5
+        // 6'd35: divider_value_o = 24'd10213;  // D5
+        // 6'd36: divider_value_o = 24'd12151;  // B4
+        // 6'd37: divider_value_o = 24'd0;
+        // 6'd38: divider_value_o = 24'd0;
+        // 6'd39: divider_value_o = 24'd11465;  // C5
+        // 6'd40: divider_value_o = 24'd15306;  // G4
+        // 6'd41: divider_value_o = 24'd18196;  // E4
+        // 6'd42: divider_value_o = 24'd13636;  // A4
+        // 6'd43: divider_value_o = 24'd0;
+        // 6'd44: divider_value_o = 24'd13636;  // A4
+        // 6'd45: divider_value_o = 24'd13636;  // A4
+        // 6'd46: divider_value_o = 24'd0;
+        // 6'd47: divider_value_o = 24'd15306;  // G4
+        // 6'd48: divider_value_o = 24'd0;
+        // 6'd49: divider_value_o = 24'd0;
+        // 6'd50: divider_value_o = 24'd9097;   // E5
+        // 6'd51: divider_value_o = 24'd11465;  // C5
+        // 6'd52: divider_value_o = 24'd12151;  // B4
+        // 6'd53: divider_value_o = 24'd13636;  // A4
+        // 6'd54: divider_value_o = 24'd15306;  // G4
+        // 6'd55: divider_value_o = 24'd7648;   // G5
+        // 6'd56: divider_value_o = 24'd9097;   // E5
+        // 6'd57: divider_value_o = 24'd0;
+        // 6'd58: divider_value_o = 24'd11465;  // C5
+        // 6'd59: divider_value_o = 24'd12151;  // B4
+        // 6'd60: divider_value_o = 24'd13636;  // A4
+        // 6'd61: divider_value_o = 24'd15306;  // G4
+        // 6'd62: divider_value_o = 24'd0;
+        // 6'd63: divider_value_o = 24'd0;
 
         default: divider_value_o = 24'd0;
     endcase
